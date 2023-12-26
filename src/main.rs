3ccd0d5a0d86a26cd7c692b3bba6fa9d723f38c9
Santo_ <santigio2003@gmail.com>
2023-12-26 11:34:49 +0100

    Implemented markdown to html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+// File I/O
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    
+    // FILE I/O -------------------------------------------------------
+    // From rust by example
+
+    // Create a path to the desired file
+    let path = Path::new("posts/example1.md");
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+                // Using markdown library to parse the file
+        Ok(_) => println!("{}", markdown::to_html(&s)),
+    }
+
+
+
 }
